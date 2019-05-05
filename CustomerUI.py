92a77b1ffa190f2d61bbf94e081fd1c375ba6731
moralesalex89 <37023362+moralesalex89@users.noreply.github.com>
2019-05-05 10:34:06 -0700

    Account Functions

--- CustomerUI.py
+++ CustomerUI.py
@@ -159,6 +159,30 @@ class CustomerUI:
         else:
             ttk.Label(self.center, font=self.defont, text="All housekeeping hours are currently booked").grid(column=0, row=len_times+1, columnspan=2)
 
+# ____________________ACCOUNT____________________
+    def account_press(self):
+        self.clear_frames()
+        ttk.Button(self.center, text="Account Information").grid()
+        ttk.Button(self.center, text="Review Transactions").grid()
+
+    def account_info(self):
+        self.clear_frames()
+        #Can someone who used the User class more than me do this one please
+
+    def display_transactions(self, result=None, index=0, page=0):
+        self.clear_frames()
+        if result is None:
+            result = get_payment_list(self.activeUser.get_userID)
+        if page >= 10:
+            ttk.Button(self.center, text="<<", command=lambda: self.display_transactions(result, 0, page-10))
+        if (len(result) - page) >= 10:
+            ttk.Button(self.center, text=">>", command=lambda: self.display_transactions(result, 0, page+10))
+        headers = ["Payment ID", "Charge", "Info"]
+        self.display_headers(headers, 1)
+        for index in range(min(10, len(result) - page)):
+            entry = ['%s' % result[index+page][0], '%s' % result[index+page][2], '%s' % result[index+page][3]]
+            self.display_headers(entry, index)
+            
     # ____________________OTHER____________________
     def clear_frames(self):
         self.UI.clear_center()

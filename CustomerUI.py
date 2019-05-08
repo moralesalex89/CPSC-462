7c3ec2ce9e680e11174ecbd06da7f4becd243027
Ruke <michael.ruke@gmail.com>
2019-05-08 00:00:31 -0700

    quick fix in booking logic

--- CustomerUI.py
+++ CustomerUI.py
@@ -201,9 +201,9 @@ class CustomerUI:
     # if Room was not available when pressed it will prompt user there was an error
     def reserve(self,start_date,end_date,room_id):
         if self.resMan.create_reservation(self.resMan,start_date,end_date,self.activeUser.get_userID(),room_id) == True:
+            self.activeUser.login_user(self.activeUser.get_username())
             self.booking_press()
             self.UI.display_message_frame("Reservation made for %s - %s" % (start_date,end_date))
-            self.activeUser.login_user(self.activeUser.get_username())
             self.pop.destroy()
         else:
             self.UI.display_message_frame("Room Taken")
